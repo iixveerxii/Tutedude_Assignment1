dfb9f2bcb35f6c499d96dce8de4237239e76fb0a
iixveerxii <141078676+iixveerxii@users.noreply.github.com>
2025-12-11 04:33:45 +0530

    Add files via upload

---
+++ Assignment1_Task1.py
@@ -0,0 +1,12 @@
+n1 = float(input("enter first number: "))
+n2 = float(input("enter second number: "))
+
+add = n1+n2
+sub = n1-n2
+multi = n1*n2
+div = n1/n2
+
+print("Addition:",add)
+print("Subtraction:",sub)
+print("Multiplication:",multi)
+print("Division:",div)
